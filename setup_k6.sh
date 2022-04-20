593d9d52783c9942a13b0b4d2418c8ac64221bbd
hamden007 <hamden007@gmail.com>
2022-04-20 13:24:22 +0700

    update setup_k6

--- setup_k6.sh
+++ setup_k6.sh
@@ -11,3 +11,4 @@ sudo apt install curl
 sudo curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs
+sudo npm install -g k6-to-junit
